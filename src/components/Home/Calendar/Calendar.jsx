fabc0d9610e2daee4b83215aad6e163338cc0e1a
MShmatok <91060161+MShmatok@users.noreply.github.com>
2023-11-16 23:17:43 +0200

    fix bugs calendar / Button

--- src/components/Home/Calendar/Calendar.jsx
+++ src/components/Home/Calendar/Calendar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Today from './Today/Today';
 import DateSelector from './DateSelector/DateSelector';
 import { CalendarWrap } from './Calendar.styled';
-import { Button } from 'CommonStyle/Button/Button.styled';
+// import { Button } from 'CommonStyle/Button/Button.styled';
 
 const Calendar = () => {
   return (
