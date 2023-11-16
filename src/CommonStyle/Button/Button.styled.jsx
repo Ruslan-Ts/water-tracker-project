ad3cb95dfab6f5548a15ee0e3fe7d2c5f4c11cd5
mrcolti4 <kbulygin456@gmail.com>
2023-11-16 09:10:33 +0200

    Add signlayout pictures for retina display, fix styles

--- src/CommonStyle/Button/Button.styled.jsx
+++ src/CommonStyle/Button/Button.styled.jsx
@@ -12,13 +12,22 @@ export const Button = styled.button`
   outline: none;
   border: none;
   border-radius: 10px;
-  padding: 8px 30px;
   cursor: pointer;
+  width: 280px;
+  height: 36px;
   &:hover {
     box-shadow: 0px 4px 14px 0px rgba(64, 123, 255, 0.54);
   }
   @media screen and (min-width: 768px) {
     font-size: 18px;
     line-height: calc(24 / 18);
+    width: 336px;
+    height: 44px;
+  }
+  @media screen and (min-width: 1440px) {
+    font-size: 18px;
+    line-height: calc(24 / 18);
+    width: 384px;
+    height: 44px;
   }
 `;
