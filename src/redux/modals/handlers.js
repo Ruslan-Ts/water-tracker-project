1872b0cd50683c32be88f0763713da2b9be97def
DariaKuzmenko <103844653+DariaKuzmenko@users.noreply.github.com>
2023-11-19 22:56:00 +0100

    merge main

--- src/redux/modals/handlers.js
+++ src/redux/modals/handlers.js
@@ -1,6 +1,12 @@
 export const handlerCloseModalSetting = (state, { payload }) => {
-    state.isOpenModalSetting = false;
-}
+  state.isOpenModalSetting = false;
+};
 export const handlerCloseModalWaterRate = (state, { payload }) => {
-    state.isOpenModalWaterRate = false;
-}
+  state.isOpenModalWaterRate = false;
+};
+export const handlerCloseModalAddWater = (state, { payload }) => {
+  state.isOpenAddWaterModal = false;
+};
+export const handlerCloseModalDeleteEntry = (state, { payload }) => {
+  state.isOpenDeleteEntryModal = false;
+};
