54aec600ae896418f70f7ed36d9549f3a1da9b9c
Nadia Alaieva <tizzifona@gmail.com>
2023-11-20 16:23:03 +0100

    Changed main logo

--- src/components/HeaderLogo/HeaderLogo.jsx
+++ src/components/HeaderLogo/HeaderLogo.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Icon from '../../img/main-logo.svg';
+import { Logo, LogoIcon } from './HeaderLogo.styled';
 
 const HeaderLogo = () => (
     <Link to={'/'}>
-      <img src={Icon} alt="Logo" />
+      <Logo>
+        <LogoIcon src={Icon} alt="Logo" />
+        <span>
+          Tracker <br /> of water
+        </span>
+      </Logo>
     </Link>
 );
 
 export default HeaderLogo;
+
